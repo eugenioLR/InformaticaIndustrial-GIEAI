a15fea6f1c95df12ba9bf7251d08e11e8d7cd08a
Eugenio Lorente <46348947+eugenioLR@users.noreply.github.com>
2026-03-24 11:55:21 +0100

    Actualizados comentarios en Practica 2

--- Practica2/main.c
+++ Practica2/main.c
@@ -11,11 +11,10 @@ uint16_t arr_primos[N_PRIMOS_16BITS];
 	Nota: cada iteracion consume 5 ciclos de reloj (5 instrucciones en MCU).
 */
 void delay(uint32_t ticks){
-	// Declaramos el contador como una variable
+	// Declaramos el contador como una variable 'volatile' para evitar que el compilador la elimine.
 	volatile uint32_t i = 0;
 	for(i = 0; i < ticks; i++);
 }
-
 
 /*
 	Decide si un numero es primo o no en tiempo O(sqrt(n)).
@@ -26,7 +25,7 @@ uint8_t es_primo(uint16_t numero)
 	
 	// Si el numero es menor de 16 ya sabemos si es primo o no sin comprobar nada solo usando un uint16_t.
 	// Comprobar si 'numero' es igual a 2, 3, 5, 7, 11 o 13 usando operaciones de bits (1 no es primo):
-	// · Mueve un bit a la posicion 'numero' empezando en 0 y lo maskea para encontrar si es uno de los numeros que buscamos.
+	// Â· Mueve un bit a la posicion 'numero' empezando en 0 y lo maskea para encontrar si es uno de los numeros que buscamos.
 	// Nota: 0x28AC = 0b0010100010101100
 	if (numero < 16)
 		return ((1 << numero) & 0x28AC) != 0;
@@ -38,7 +37,7 @@ uint8_t es_primo(uint16_t numero)
 	
 	// Busca todos los numeros impares para encontrar si alguno es divisor de 'numero'
 	// Busca hasta sqrt(numero).
-	// · i < sqrt(numero) <=> i*i < numero. 	Con (numero > 0) y (i > 0)
+	// Â· i < sqrt(numero) <=> i*i < numero. 	Con (numero > 0) y (i > 0)
 	for (i = 3; i*i < numero; i+=2)
 		if (!(numero%i))
 			return 0;
@@ -59,7 +58,7 @@ int main()
 	{
 		if(es_primo(i))
 		{
-			arr_primos[cursor_arr] = i; 			// Si i es primo lo añadimos al array
+			arr_primos[cursor_arr] = i; 			// Si i es primo lo aÃ±adimos al array
 			cursor_arr++;												// Incrementamos el cursor
 		}
 	}
